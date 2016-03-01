c43cf5da16c89ae819be6f5cfa142a0860ca7f2e
muzhilong <laomu1988@qq.com>
2016-03-01 10:29:38 +0800

    简化math

--- math.js
+++ math.js
@@ -3,235 +3,105 @@
  * 例如：
  * 1 + 1 正确,1 * 2 正确, 1 * 2(3) 错误, 1 * 2+(3) 正确， 1 * 2 + (3*)*2 错误
  * */
+function getType(cha) {
+    if (!cha) {
+        return false;
+    }
+    if (cha >= '0' && cha <= '9') {
+        return 'number';
+    }
+    switch (cha) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            return 'operator';
+        case '(':
+        case ')':
+        case ' ':
+            return cha;
+    }
+    return 'unknown';
+}
 
-
-
-function Test(str) {
+function Diagram(str) {
     if (!str) {
         return false;
     }
-    var place = 0, cha = '', nowType = 0;
+    str += '';
+    var len = str.length, nowType = '', place = 0, word = '';
 
-    function eat() {
-        if (place < str.length) {
-            cha = str.charAt(place);
-            place += 1;
-            nowType = cha >= '0' && cha <= '9' ? 'number' : isOpeator(cha) ? 'operator' : cha;
-            return true;
-        } else {
-            place = -1;
-            cha = '';
-            nowType = '';
-            return false;
-        }
-    }
-
-    function skipSpace() {
-        while (cha == ' ') {
-            eat();
-        }
-    }
-
-    function eatNumber() {
-        while (nowType === 'number') {
-            eat();
-        }
-    }
-
-    function isOpeator(cha) {
-        switch (cha) {
-            case '+':
-            case '-':
-            case '*':
-            case '/':
-                return true;
-        }
-        return false;
-    }
-
-    // number | ( expr )
-    function bracket() {
-
-    }
-
-    // bracket ( operator bracket) *
-    function cal() {
-
-    }
-
-    /**
-     * ( expr ) operator expr
-     *  number [operator expr]
-     * */
-    function expr() {
-        if (nowType == ' ') {
-            skipSpace();
-        }
-        if (nowType == '(') {
-            eat();
-            expr();
-            skipSpace();
-            if (nowType === ')') {
-                eat();
-                skipSpace();
-                if (cha) {
-                    if (isOpeator(cha)) {
-                        eat();
-                        return expr();
-                    }
-                    console.log('error need to be operator');
-                } else {
-                    return true;
-                }
-            } else {
-                console.log('error: need ")"');
-            }
-        } else if (nowType == 'number') {
-            eatNumber();
-            skipSpace();
-            if (cha) {
-                if (isOpeator(cha)) {
-                    eat();
-                    return expr();
-                }
-                else {
-                    console.log('unexpected char "' + cha + '"');
-                    return false;
-                }
-            }
-            return true;
-        }
-        console.log('unexpected char "' + cha + '"');
-        return false;
-    }
-
-    eat();
-    return expr();
-}
-function Diagram(str) {
-    this.init(str);
-}
-Diagram.prototype = {
-    str: '',
-    nowType: '',
-    len: 0,
-    place: 0,
-    word: '',
-    init: function (str) {
-        if (str) {
-            str += '';
-            this.str = str;
-            this.len = str.length;
-        }
-        return this;
-    },
     // 读取一个数或者一个操作符，空格自动切换到下一个
-    read: function () {
-        if (this.place < this.len) {
-            var cha = this.str.charAt(this.place);
-            this.nowType = this.getType(cha);
-            this.word = cha;
-            if (this.nowType && this.nowType == ' ') {
-                this.place += 1;
+    function getNextType() {
+        if (place < len) {
+            var cha = str.charAt(place);
+            nowType = getType(cha);
+            word = cha;
+            if (nowType && nowType == ' ') {
+                place += 1;
                 // 空格自动切换到下一个
-                return this.read();
+                return getNextType();
             }
-            if (this.nowType === 'number') {
+            if (nowType === 'number') {
                 var i = 1;
-                while ((cha = this.str.charAt(this.place + i)) && this.getType(cha) == this.nowType) {
-                    this.word += cha;
+                while ((cha = str.charAt(place + i)) && getType(cha) == nowType) {
+                    word += cha;
                     i += 1;
                 }
             }
-            return this.nowType;
+            return nowType;
         } else {
-            this.word = '';
-            this.nowType = 'end';
+            word = '';
+            nowType = 'end';
             return 'end';
         }
-    },
-    process: function () {
-        if (this.word) {
-            this.place += this.word.length;
-        } else {
-            console.error('error');
+    }
 
-        }
-    },
-    // 1 ( + 1 )
-    readLineNumber: function () {
-        if (this.nowType == 'number') {
-            this.process();
+    function process() {
+        if (word) {
+            place += word.length;
         } else {
             console.error('error');
         }
-        this.read();
-        if (this.nowType == 'end') {
-            return true;
-        }
-        else if (this.nowType == 'operator') {
-            this.process();
-            this.readExpr();
-        }
-    },
+    }
+
+
     // 带括号的表达式 ( 1 + 1 ) * 1
-    readExpr: function () {
-        this.read();
-        if (this.nowType == 'end') {
+    function readExpr() {
+        getNextType();
+        if (nowType == 'end') {
             console.log('未预料到的结尾！');
+            return false;
         }
-        else if (this.nowType == '(') {
-            this.process();
-            this.readExpr();
-            this.read();
-            if (this.nowType == ')') {
-                this.process();
+        else if (nowType == '(') {
+            process();
+            readExpr();
+            getNextType();
+            if (nowType == ')') {
+                process();
                 return true;
             } else {
                 console.error('缺少右括号');
                 return false;
             }
-        } else if (this.nowType == 'number') {
-            this.process();
-            this.read();
-            if (this.nowType == 'end') {
+        } else if (nowType == 'number') {
+            process();
+            getNextType();
+            if (nowType == 'end') {
                 return true;
-            } else if (this.nowType == 'operator') {
-                this.process();
-                return this.readExpr();
+            } else if (nowType == 'operator') {
+                process();
+                return readExpr();
             }
             return false;
         } else {
-            console.error('未预料到的类型：', this.nowType);
+            console.error('未预料到的类型：', nowType);
             return false;
         }
-    },
-    test: function () {
-        console.log(this.str);
-        return this.readExpr();
-    },
-    getType: function (cha) {
-        if (!cha) {
-            return false;
-        }
-        if (cha >= '0' && cha <= '9') {
-            return 'number';
-        }
-        switch (cha) {
-            case '+':
-            case '-':
-            case '*':
-            case '/':
-                return 'operator';
-            case '(':
-            case ')':
-            case ' ':
-                return cha;
-        }
-        return 'unknown';
     }
-};
 
+    return readExpr();
+}
 // 正则格式化算式
 function format(str) {
     return str.replace(/\s*(\d+|[\+\-\*\/\{\}])\s*/g, function (cha) {
@@ -240,5 +110,6 @@ function format(str) {
 }
 
 module.exports = function (str) {
-    console.log((new Diagram(str)).test());
-};
+    console.log('string:', str);
+    console.log('result:', Diagram(str));
+}
